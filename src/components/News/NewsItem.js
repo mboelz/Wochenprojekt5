121ca40682cb5ee488bb9abccbf1a7626c3a61e4
n-els <nels@tuta.io>
2022-10-28 15:47:58 +0200

    responsiveness, search results quantity

--- src/components/News/NewsItem.js
+++ src/components/News/NewsItem.js
@@ -1,35 +1,45 @@
 import React from 'react';
-import styles from './NewsItem.module.css'
-import {FaUserAlt} from 'react-icons/fa'
-import {VscActivateBreakpoints} from 'react-icons/vsc'
-import {BsCalendarDateFill} from 'react-icons/bs'
-
+import styles from './NewsItem.module.css';
+import { FaUserAlt } from 'react-icons/fa';
+import { VscActivateBreakpoints } from 'react-icons/vsc';
+import { BsCalendarDateFill } from 'react-icons/bs';
 
 const NewsItem = ({ id, title, author, date, url, points }) => {
-
-  const convertedDate = new Date(date).toDateString()
-
+  const convertedDate = new Date(date).toDateString();
 
   return (
     <div className={styles.NewsItem}>
+      <div className={styles['NewsItem-ID']}>
+        <span>{id}</span>
+      </div>
 
-
-        <div className={styles["NewsItem-ID"]}>
-         <span>{id}</span>
+      <div className={styles['NewsItem-Text']}>
+        <a href={url} target="_blank">
+          <div className={styles['title-bold']}>{title}</div>
+        </a>
+        <div className={styles['NewsItem-Text-Info']}>
+          <div>
+            <span>
+              <FaUserAlt />
+            </span>
+            {author}
+          </div>
+          <div className={styles.points}>
+            <span>
+              <VscActivateBreakpoints />
+            </span>
+            {points}
+          </div>
+          <div>
+            <span>
+              <BsCalendarDateFill />
+            </span>
+            {convertedDate}
+          </div>
         </div>
-
-        <div className={styles["NewsItem-Text"]}>
-          <a href={url} target="_blank">
-          <div className={styles["title-bold"]}>{title}</div>
-          </a>
-          <div className={styles["NewsItem-Text-Info"]}>
-          <div><span><FaUserAlt/></span>{author}</div>
-          <div><span><VscActivateBreakpoints/></span>{points}</div>
-          <div><span><BsCalendarDateFill/></span>{convertedDate}</div>
-        </div>
+      </div>
+      {/* </a> */}
     </div>
-        {/* </a> */}
-    </div >
   );
 };
 
--- src/components/News/Searchbar.js
+++ src/components/News/Searchbar.js
@@ -1,37 +1,78 @@
 import React, { useState } from 'react';
-import {IoSearchCircle} from 'react-icons/io5'
-import styles from './Searchbar.module.css'
+import { IoSearchCircle } from 'react-icons/io5';
+import styles from './Searchbar.module.css';
 
-const Searchbar = ({ setUrl, numberOfArticles }) => {
-
+const Searchbar = ({ setUrl, numberOfArticles, setNumberOfArticles }) => {
   // variable für React State
-  const [inputValue, setInputValue] = useState("")    // "" string, {} objekt
+  const [inputValue, setInputValue] = useState(''); // "" string, {} objekt
 
   function handleChange(event) {
-
     // value aus inputfield immer über event.target.value
-    setInputValue(event.target.value)
-
-
+    setInputValue(event.target.value);
   }
 
   function handleClick() {
     // inputValue State eine Komponente höher schicken
-    if (inputValue === "") {
-      setUrl(`https://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=${numberOfArticles}`)
+    if (inputValue === '') {
+      setUrl(
+        `https://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=${numberOfArticles}`
+      );
     } else {
-      setUrl(`https://hn.algolia.com/api/v1/search?query=${inputValue}&hitsPerPage=${numberOfArticles}`)
+      setUrl(
+        `https://hn.algolia.com/api/v1/search?query=${inputValue}&hitsPerPage=${numberOfArticles}`
+      );
     }
   }
+
+  function handleQuantityRadio(e) {
+    // console.log(e.target.value);
+    setNumberOfArticles(e.target.value);
+  }
+
+  console.log(numberOfArticles);
 
   // https://hn.algolia.com/api/v1/search_by_date?query=${inputValue}
 
   return (
-    <div className={styles.searchbar}>
-      <input type="text" placeholder='Suche...' onChange={handleChange} />
-      <button className={styles.btn} onClick={handleClick}><IoSearchCircle/></button>
+    <div>
+      <div className={styles.searchbar}>
+        <input type="text" placeholder="Suche..." onChange={handleChange} />
+        <button className={styles.btn} onClick={handleClick}>
+          <IoSearchCircle />
+        </button>
+      </div>
+      <div className={styles.radioResult}>
+        <h3>Suchergebnisse: </h3>
+        <label className={styles.labelRadioResult}>
+          <input
+            type="radio"
+            name="ergebnisse"
+            value="10"
+            onChange={handleQuantityRadio}
+          ></input>
+          10
+        </label>
+        <label className={styles.labelRadioResult}>
+          <input
+            type="radio"
+            name="ergebnisse"
+            value="50"
+            onChange={handleQuantityRadio}
+          ></input>
+          50
+        </label>
+        <label className={styles.labelRadioResult}>
+          <input
+            type="radio"
+            name="ergebnisse"
+            value="100"
+            onChange={handleQuantityRadio}
+          ></input>
+          100
+        </label>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Searchbar;
